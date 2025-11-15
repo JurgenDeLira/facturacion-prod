8835130168ac32f5554541226b6c96fcdb87e0b5
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-14 20:48:16 -0700

    concectando con mysql en application.properties

--- src/main/java/com/jorge/facturacion_prod/FacturacionProdApplication.java
+++ src/main/java/com/jorge/facturacion_prod/FacturacionProdApplication.java
@@ -8,10 +8,6 @@ public class FacturacionProdApplication {
 
 	public static void main(String[] args) {
 
-		Producto p = new Producto(2, "Computador");
-		p.setNombre("Televisor");
-
-		p.getNombre();
 
 		SpringApplication.run(FacturacionProdApplication.class, args);
 	}
