291c5faa934e7e335acd6fe2e0f6449c2e826935
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-17 12:28:45 -0700

    CRUD en ProductoService

--- src/main/java/com/jorge/facturacion_prod/service/ProductoService.java
+++ src/main/java/com/jorge/facturacion_prod/service/ProductoService.java
@@ -1,8 +1,14 @@
 package com.jorge.facturacion_prod.service;
 
+import com.jorge.facturacion_prod.dto.ProductoDto;
+import com.jorge.facturacion_prod.model.Producto;
 import com.jorge.facturacion_prod.repository.ProductoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductoService {
@@ -14,5 +20,51 @@ public class ProductoService {
     public ProductoService(ProductoRepository productoRepository, ModelMapper modelMapper) {
         this.productoRepository = productoRepository;
         this.modelMapper = modelMapper;
+    }
+
+    //save
+    public ProductoDto save (ProductoDto productoDto){
+        Producto producto = modelMapper.map(productoDto, Producto.class); //transformamos DTO a producto
+        return modelMapper.map(productoRepository.save(producto), ProductoDto.class)
+
+    }
+
+    //find all
+    public List<ProductoDto> findAll(){
+        return productoRepository.findAll().stream().map(
+                producto -> {
+                    return modelMapper.map(producto, ProductoDto.class);
+                }
+        ).collect(Collectors.toList());
+    }
+
+    //find by id
+    public Optional<ProductoDto> findById(Integer id){
+        return productoRepository.findById(id).map(
+                producto -> {
+                    return modelMapper.map(producto, ProductoDto.class);
+                }
+        );
+    }
+
+    //deleteById
+    public boolean deleteBy (Integer id){
+        return productoRepository.findById(id).map(
+                producto -> {
+                    productoRepository.delete(producto);
+                    return true;
+                }
+        ).orElse(false);
+    }
+
+    //update
+    public Optional<ProductoDto> update(ProductoDto productoDto){
+        Producto producto = modelMapper.map(productoDto, Producto.class);
+        return productoRepository.findById(producto.getId()).map(
+                productoDB -> {
+                    return modelMapper.map(productoRepository.save(producto), ProductoDto.class);
+                }
+        );
+
     }
 }
