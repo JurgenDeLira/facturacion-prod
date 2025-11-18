2c7fb3a71ab979af0cdc11b19a9ca6dff89e564e
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-18 11:34:32 -0700

    cargar datos en BDD mediante un archivo SQL

---
+++ src/main/resources/sql/custom-data.sql
@@ -0,0 +1,6 @@
+USE `facturacion-prod`;
+INSERT INTO productos (nombre, detalle, precio, fecha_creado)
+VALUES
+('Mouse inalámbrico M185', 'Detalle del producto1', 350.00, '2025-02-16 01:22:00'),
+('Computadora de escritorio', 'Detalle del producto2', 17000.00, '2025-02-16 01:22:00'),
+('Producto3', "Detalle del producto3", 200.00, '2025-02-16 01:22:00');
