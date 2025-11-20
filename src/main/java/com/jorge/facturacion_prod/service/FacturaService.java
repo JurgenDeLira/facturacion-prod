07712e8a8bc623fffc7629382d4e60171572fa41
Jürgen De Lira <arq.jorgedelira@gmail.com>
2025-11-19 18:56:34 -0700

    logica de negocio para guardar una factura

--- src/main/java/com/jorge/facturacion_prod/service/FacturaService.java
+++ src/main/java/com/jorge/facturacion_prod/service/FacturaService.java
@@ -1,14 +1,23 @@
 package com.jorge.facturacion_prod.service;
 
 
+import com.jorge.facturacion_prod.dto.RequestDetalleFacturaDTO;
+import com.jorge.facturacion_prod.dto.RequestFacturaDTO;
 import com.jorge.facturacion_prod.dto.ResponseFacturaDTO;
+import com.jorge.facturacion_prod.model.DetalleFactura;
+import com.jorge.facturacion_prod.model.Factura;
+import com.jorge.facturacion_prod.model.Producto;
 import com.jorge.facturacion_prod.repository.FacturaRepository;
 import com.jorge.facturacion_prod.repository.ProductoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -44,5 +53,43 @@ public class FacturaService {
     //Eliminar las facturas por su id
     public void deleteById(Integer id) {
         facturaRepository.deleteById(id);
+    }
+
+    @Transactional
+    public ResponseFacturaDTO save(RequestFacturaDTO requestFacturaDTO) {
+        Factura factura = new Factura();
+        BigDecimal subtotalFactura = BigDecimal.ZERO;
+        Set<DetalleFactura> detalles = new HashSet<>();
+
+        factura.setNumeroFactura(requestFacturaDTO.getNumeroFactura());
+
+        for (RequestDetalleFacturaDTO detalleFacturaDTO: requestFacturaDTO.getDetalleFacturas()){
+            //Producto producto = productoRepository.findById(detalleFacturaDTO.getIdProducto()).get();
+            Producto producto = productoRepository.findById(detalleFacturaDTO.getIdProducto()).orElseThrow(
+                    () -> new RuntimeException(" Producto no encontrado")
+            );
+
+            BigDecimal totalProducto = producto.getPrecio().multiply(BigDecimal.valueOf(detalleFacturaDTO.getCantidad()));
+
+            subtotalFactura = subtotalFactura.add(totalProducto);
+
+            DetalleFactura detalleFactura = new DetalleFactura();
+            detalleFactura.setIdProducto(detalleFacturaDTO.getIdProducto());
+            detalleFactura.setPrecio(producto.getPrecio());
+            detalleFactura.setCantidad(detalleFacturaDTO.getCantidad());
+            detalleFactura.setTotal(totalProducto);
+            detalleFactura.setFactura(factura);
+
+            detalles.add(detalleFactura);
+        }
+
+        factura.setDetalleFacturas(detalles);
+        factura.setSubTotal(subtotalFactura);
+        factura.setTotal(subtotalFactura.add(subtotalFactura
+                .multiply(BigDecimal.valueOf(factura.getIVA()))));
+
+        Factura savedFactura = facturaRepository.save(factura);
+
+        return modelMapper.map(savedFactura, ResponseFacturaDTO.class);
     }
 }
